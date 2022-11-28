36e7a7ab90c7d1769656c6ea4313a1868114b40a
ofirsnb <ofirsnb@gmail.com>
2022-11-28 22:54:00 +0200

    added `start` and `stop`...
    removed token

--- src/server/routes/containers.ts
+++ src/server/routes/containers.ts
@@ -1,16 +1,7 @@
-import {FastifyInstance, FastifyRequest} from 'fastify';
+import {FastifyInstance} from 'fastify';
 import {dockerCommand} from 'docker-cli-js';
-import {TOKEN} from '../const';
-
-const authMiddleware = async (request: FastifyRequest): Promise<void> => {
-  if(request.headers['token'] !== TOKEN) {
-    request.log.error({msg: 'Unauthorized request', headers: request.headers});
-    throw { code: 401, message: 'Unauthorized request' };
-  }
-};
 
 export default async (server: FastifyInstance): Promise<void> => {
-  server.addHook('preHandler', authMiddleware);
 
   server.get('/', async (request, reply) => {
     const docker = await dockerCommand('ps -a', {});
@@ -20,6 +11,18 @@ export default async (server: FastifyInstance): Promise<void> => {
   server.get('/logs/:id', async (request, reply) => {
     const {id} = request.params as unknown as { id: string };
     const docker = await dockerCommand(`logs -n 15 ${id}`, {});
+    reply.send(docker);
+  });
+
+  server.get('/start/:id', async (request, reply) => {
+    const {id} = request.params as unknown as { id: string };
+    const docker = await dockerCommand(`start ${id}`, {});
+    reply.send(docker);
+  });
+
+  server.get('/stop/:id', async (request, reply) => {
+    const {id} = request.params as unknown as { id: string };
+    const docker = await dockerCommand(`stop ${id}`, {});
     reply.send(docker);
   });
 };
